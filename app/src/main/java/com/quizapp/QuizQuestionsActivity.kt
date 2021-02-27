069cfaaf7876644b13b0256f54c12a30c4dfd2ea
gunish.jha <gunish.jha.master@Gmail.com>
2021-02-27 18:33:56 +0530

    feat: Added the submit button functionality to go to the next question.

--- app/src/main/java/com/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/quizapp/QuizQuestionsActivity.kt
@@ -5,22 +5,17 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
-    // TODO (STEP 2: A global variables for current position and questions list.)
-    // START
     private var mCurrentPosition: Int = 1 // Default and the first question position
     private var mQuestionsList: ArrayList<Question>? = null
-    // END
 
-    // TODO (STEP 5: A global variables for selected option.)
-    // START
     private var mSelectedOptionPosition: Int = 0
-    // END
 
     /**
      * This function is auto created by Android when the Activity Class is created.
@@ -31,19 +26,18 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_quiz_questions)
 
-        // TODO (STEP 3: Make the questions list and the current position variable global and remove the logs here.)
-        // START
         mQuestionsList = Constants.getQuestions()
-        // END
 
         setQuestion()
 
-        // TODO (STEP 4: Set all the click events for Options using the interface onClick listener)
-        // START
         tv_option_one.setOnClickListener(this)
         tv_option_two.setOnClickListener(this)
         tv_option_three.setOnClickListener(this)
         tv_option_four.setOnClickListener(this)
+
+        // TODO(STEP 1: Adding a click event for submit button.)
+        // START
+        btn_submit.setOnClickListener(this)
         // END
     }
 
@@ -70,20 +64,66 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
                 selectedOptionView(tv_option_four, 4)
             }
+
+            // TODO(STEP 2: Adding a click event for submit button. And change the questions and check the selected answers.)
+            // START
+            R.id.btn_submit -> {
+
+                if (mSelectedOptionPosition == 0) {
+
+                    mCurrentPosition++
+
+                    when {
+
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+
+                            setQuestion()
+                        }
+                        else -> {
+
+                            Toast.makeText(this@QuizQuestionsActivity, "You have successfully completed the quiz.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+
+                    // This is to check if the answer is wrong
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+
+                    // This is for correct answer
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if (mCurrentPosition == mQuestionsList!!.size) {
+                        btn_submit.text = "FINISH"
+                    } else {
+                        btn_submit.text = "GO TO NEXT QUESTION"
+                    }
+
+                    mSelectedOptionPosition = 0
+                }
+            }
         }
     }
 
-    // TODO (STEP 1 : Lets create a function to set the question in the UI components which we have done earlier the onCreate method. And make some of the variables global which we will be using later.)
-    // START
     /**
      * A function for setting the question to UI components.
      */
     private fun setQuestion() {
 
-        val question =
-            mQuestionsList!!.get(mCurrentPosition - 1) // Getting the question from the list with the help of current position.
+        val question = mQuestionsList!!.get(mCurrentPosition - 1) // Getting the question from the list with the help of current position.
 
         defaultOptionsView()
+
+        // TODO (STEP 6: Check here if the position of question is last then change the text of the button.)
+        // START
+        if (mCurrentPosition == mQuestionsList!!.size) {
+            btn_submit.text = "FINISH"
+        } else {
+            btn_submit.text = "SUBMIT"
+        }
+        // END
 
         progressBar.progress = mCurrentPosition
         tv_progress.text = "$mCurrentPosition" + "/" + progressBar.getMax()
@@ -95,10 +135,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tv_option_three.text = question.optionThree
         tv_option_four.text = question.optionFour
     }
-    // END
 
-    // TODO (STEP 6: Create a function for view for highlighting the selected option.)
-    // START
     /**
      * A function to set the view of selected option view.
      */
@@ -118,8 +155,6 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         )
     }
 
-    // TODO (STEP 8: Create a function to set default options view.)
-    // START
     /**
      * A function to set default options view when the new question is loaded or when the answer is reselected.
      */
@@ -140,4 +175,41 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             )
         }
     }
+
+    // TODO (STEP 3: Create a function for answer view.)
+    // START
+    /**
+     * A function for answer view which is used to highlight the answer is wrong or right.
+     */
+    private fun answerView(answer: Int, drawableView: Int) {
+
+        when (answer) {
+
+            1 -> {
+                tv_option_one.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+            2 -> {
+                tv_option_two.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+            3 -> {
+                tv_option_three.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+            4 -> {
+                tv_option_four.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+        }
+    }
+    // END
 }
